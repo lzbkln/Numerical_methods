ee0a0d6ced29442d390df7f85056defcc2116349
lzbkl <lzbkln@mail.ru>
2025-05-23 18:04:41 +0500

    fix first page

--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -2,5 +2,5 @@ let startButton = document.getElementById("startButtonId");
 
 startButton.addEventListener("click", function(e) {
   e.preventDefault();
-  window.location.href = 'pages/page2.html';
+  window.location.href = '/groups';
 });
